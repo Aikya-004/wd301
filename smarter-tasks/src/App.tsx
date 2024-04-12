173506a1a3ce94e949027bfc0fe817e2b7f5dd3f
aikya <aikyaana004@gmail.com>
2024-04-12 11:07:19 +0530

    Repo Structure changed

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,56 +1,94 @@
-import './App.css'
+// import './App.css'
+// import {
+//   createBrowserRouter,
+//   Navigate,
+//   RouterProvider,
+// } from "react-router-dom";
+// import HomePage from './pages/HomePage';
+// import TaskListPage from './pages/TaskListPage';
+// import TaskDetailsPage from "./pages/TaskDetailsPage";
+// import Form from './Form';
+// import ReactPlayground from './ReactPlayground';
+// import Layout from "./Layout";
+// import Signin from "./pages/Signin";
+// import ProtectedRoute from "./ProtectedRoute";
+// import Notfound from "./pages/Notfound"
+// // import TaskApp from "./TaskApp";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Navigate to="/signin" replace />,
+//   },
+//   {
+//     path: "/signin",
+//     element: <Signin />,
+//   },
+//   {
+//     element: (
+//       <ProtectedRoute>
+//         <Layout />
+//       </ProtectedRoute>
+//     ),
+//     children: [
+//       {
+//         path: "home",
+//         element: <HomePage />,
+//       },
+//       {
+//         path: "tasks",
+//         element: <TaskListPage />,
+//       },
+//       {
+//         path: "tasks/:id",
+//         element: <TaskDetailsPage />,
+//       },
+//     ],
+//   },
+//   { 
+//     path: "/notfound", 
+//     element: <Notfound /> 
+//   },
+//   { 
+//     path: "*", 
+//     element: <Navigate to="/notfound" replace />,
+//   },
+// ]);
+
+// const App = () => {
+//   return (
+//     <>
+//       <ReactPlayground />
+//       <RouterProvider router={router} />
+//     </>
+//   );
+// }
+
+// export default App
 import {
   createBrowserRouter,
-  Navigate,
   RouterProvider,
 } from "react-router-dom";
-import HomePage from './pages/HomePage';
-import TaskListPage from './pages/TaskListPage';
-import TaskDetailsPage from "./pages/TaskDetailsPage";
-import Layout from "./Layout";
-import Signin from "./pages/Signin";
-import ProtectedRoute from "./ProtectedRoute";
-import Notfound from "./pages/Notfound"
-// import TaskApp from "./TaskApp";
+import Notfound from "./pages/Notfound";
+import Signup from './pages/signup';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to="/signin" replace />,
+    element: <Signup />,
   },
   {
-    path: "/signin",
-    element: <Signin />,
+    path: "/signup",
+    element: <Signup />,
   },
   {
-    element: (
-      <ProtectedRoute>
-        <Layout />
-      </ProtectedRoute>
-    ),
-    children: [
-      {
-        path: "home",
-        element: <HomePage />,
-      },
-      {
-        path: "tasks",
-        element: <TaskListPage />,
-      },
-      {
-        path: "tasks/:id",
-        element: <TaskDetailsPage />,
-      },
-    ],
+    path: "/notfound",
+    element: <Notfound />,
   },
-  { 
-    path: "/notfound", 
-    element: <Notfound /> 
-  },
-  { 
-    path: "*", 
-    element: <Navigate to="/notfound" replace />,
-  },
+  {
+    path: "*",
+    element: <Notfound />,
+  }
 ]);
 
 const App = () => {
